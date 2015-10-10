4f2cabb669f5b7bdc7cd57eac9488dcf2b745cc3
piyush <piyushthapa74@gmail.com>
2015-10-10 22:03:00 +0545

    added templates

--- package.js
+++ package.js
@@ -1,7 +1,18 @@
 
 Package.describe({
-    name: "wsnepal:serverStatus",
+    name: "wsnepal:serverstatus",
     summary: "",
     version: "0.0.1",
     git: "https://github.com/wsNepal/serverstatus"
+});
+Package.onUse(function(api){
+    api.versionsFrom(['METEOR@1.0']);
+    api.use(['mrt:highcharts','standard-minifiers','templating','accounts-base']);
+    api.addFiles([
+        'lib/client/configs/style.css',
+        'lib/client/templates/serverDashboard/serverDashboard.html',
+        'lib/client/templates/serverDashboard/serverDashboard.js',
+        'lib/client/templates/userCount/userCount.html'
+    ],'client');
+    api.addFiles(['lib/server/methods.js'],'server');
 });
