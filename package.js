36cb5852a66cec8c1fd2345a8d200ac4585ddc31
piyush <piyushthapa74@gmail.com>
2015-10-10 22:48:22 +0545

    added online users

--- package.js
+++ package.js
@@ -7,12 +7,25 @@ Package.describe({
 });
 Package.onUse(function(api){
     api.versionsFrom(['METEOR@1.0']);
-    api.use(['mrt:highcharts','standard-minifiers','templating','accounts-base']);
+    api.use(['mrt:highcharts','standard-minifiers','templating','accounts-base','mizzao:user-status','mongo']);
     api.addFiles([
         'lib/client/configs/style.css',
+        'lib/client/configs/highChartStyle.js',
+
         'lib/client/templates/serverDashboard/serverDashboard.html',
         'lib/client/templates/serverDashboard/serverDashboard.js',
-        'lib/client/templates/userCount/userCount.html'
+
+        'lib/client/templates/userCount/userCount.html',
+        'lib/client/templates/userCount/online/onlineUser.html',
+        'lib/client/templates/userCount/online/onlineUser.js',
     ],'client');
-    api.addFiles(['lib/server/methods.js'],'server');
+
+    api.addFiles([
+        'lib/server/methods.js',
+        'lib/server/publish.js'
+    ],'server');
+
+    api.addFiles([
+       'lib/both/collections.js'
+    ],['client','server'])
 });
